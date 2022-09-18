c3a9133383e1c8e47f43e66d21a035af3712c45b
Gabriel Mendonça <gabrielgmendonca@gmail.com>
2022-09-18 16:43:21 -0300

    Crawl songs from multiple Brazilian genres.

--- songcrawler/spiders/songsspider.py
+++ songcrawler/spiders/songsspider.py
@@ -1,10 +1,24 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
+class SongsSpider(scrapy.Spider):
     name = 'songs'
     start_urls = [
+        'https://www.letras.mus.br/estilos/axe/todosartistas.html',
+        'https://www.letras.mus.br/estilos/bossa-nova/todosartistas.html',
+        'https://www.letras.mus.br/estilos/brega/todosartistas.html',
+        'https://www.letras.mus.br/estilos/forro/todosartistas.html',
+        'https://www.letras.mus.br/estilos/funk/todosartistas.html',
+        'https://www.letras.mus.br/estilos/gospelreligioso/todosartistas.html',
+        'https://www.letras.mus.br/estilos/infantil/todosartistas.html',
+        'https://www.letras.mus.br/estilos/jovem-guarda/todosartistas.html',
+        'https://www.letras.mus.br/estilos/marchas-hinos/todosartistas.html',
         'https://www.letras.mus.br/estilos/mpb/todosartistas.html',
+        'https://www.letras.mus.br/estilos/pagode/todosartistas.html',
+        'https://www.letras.mus.br/estilos/samba/todosartistas.html',
+        'https://www.letras.mus.br/estilos/samba-enredo/todosartistas.html',
+        'https://www.letras.mus.br/estilos/sertanejo/todosartistas.html',
+        'https://www.letras.mus.br/estilos/velha-guarda/todosartistas.html',
     ]
 
     def parse(self, response):
@@ -25,16 +39,23 @@ class QuotesSpider(scrapy.Spider):
             .split(':')[1]
             .strip()
         )
+        song_lyrics = response.css('.cnt-letra p::text').getall()
         song_lyrics = '\n'.join(
-            response
-            .css('.cnt-letra p::text')
-            .getall()
+            [self._remove_extra_spaces(verse) for verse in song_lyrics]
         )
-
+        song_genre = (
+            response
+            .css('a[href*=estilos]')
+            .attrib['href']
+            .split('/')[-2]
+        )
         return {
-            'song_name': song_name,
-            'song_artist': song_artist,
-            'song_writer': song_writer,
+            'song_name': self._remove_extra_spaces(song_name),
+            'song_artist': self._remove_extra_spaces(song_artist),
+            'song_writer': self._remove_extra_spaces(song_writer),
             'song_lyrics': song_lyrics,
-            'song_genre': 'mpb',
+            'song_genre': song_genre,
         }
+
+    def _remove_extra_spaces(self, s):
+        return ' '.join(s.split())
