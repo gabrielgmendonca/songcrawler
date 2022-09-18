22655e5f8396781e15a6aa3c95f63ee0fc4b92c6
Gabriel Mendonça <gabrielgmendonca@gmail.com>
2022-09-18 08:29:32 -0300

    Get songs from top 300 MPB artists.

--- songcrawler/spiders/songsspider.py
+++ songcrawler/spiders/songsspider.py
@@ -4,59 +4,14 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'songs'
     start_urls = [
-        'https://www.letras.mus.br/chico-buarque/',
-        'https://www.letras.mus.br/milton-nascimento/',
-        'https://www.letras.mus.br/djavan/',
-        'https://www.letras.mus.br/caetano-veloso/',
-        'https://www.letras.mus.br/anavitoria/',
-        'https://www.letras.mus.br/marisa-monte/',
-        'https://www.letras.mus.br/nando-reis/',
-        'https://www.letras.mus.br/ze-ramalho/',
-        'https://www.letras.mus.br/gilberto-gil/',
-        'https://www.letras.mus.br/maria-bethania/',
-        'https://www.letras.mus.br/carlos-drummond-de-andrade/',
-        'https://www.letras.mus.br/gonzaguinha/',
-        'https://www.letras.mus.br/belchior/',
-        'https://www.letras.mus.br/elis-regina/',
-        'https://www.letras.mus.br/fagner/',
-        'https://www.letras.mus.br/toquinho/',
-        'https://www.letras.mus.br/gilsons/',
-        'https://www.letras.mus.br/ana-vilela/',
-        'https://www.letras.mus.br/seu-jorge/',
-        'https://www.letras.mus.br/vanessa-da-mata/',
-        'https://www.letras.mus.br/maria-rita/',
-        'https://www.letras.mus.br/jorge-vercillo/',
-        'https://www.letras.mus.br/maria-gadu/',
-        'https://www.letras.mus.br/adriana-calcanhotto/',
-        'https://www.letras.mus.br/jorge-ben-jor/',
-        'https://www.letras.mus.br/nelson-goncalves/',
-        'https://www.letras.mus.br/chico-cesar/',
-        'https://www.letras.mus.br/beto-guedes/',
-        'https://www.letras.mus.br/zeca-baleiro/',
-        'https://www.letras.mus.br/ana-carolina/',
-        'https://www.letras.mus.br/lenine/',
-        'https://www.letras.mus.br/oswaldo-montenegro/',
-        'https://www.letras.mus.br/ney-matogrosso/',
-        'https://www.letras.mus.br/tribalistas/',
-        'https://www.letras.mus.br/joanna/',
-        'https://www.letras.mus.br/roberta-campos/',
-        'https://www.letras.mus.br/tie/',
-        'https://www.letras.mus.br/geraldo-azevedo/',
-        'https://www.letras.mus.br/ivan-lins/',
-        'https://www.letras.mus.br/elba-ramalho/',
-        'https://www.letras.mus.br/nelsinho-correa/',
-        'https://www.letras.mus.br/gal-costa/',
-        'https://www.letras.mus.br/geraldo-vandre/',
-        'https://www.letras.mus.br/vander-lee/',
-        'https://www.letras.mus.br/marcelo-jeneci/',
-        'https://www.letras.mus.br/joao-bosco/',
-        'https://www.letras.mus.br/flavio-venturini/',
-        'https://www.letras.mus.br/marina-lima/',
-        'https://www.letras.mus.br/fafa-de-belem/',
-        'https://www.letras.mus.br/emilio-santiago/',
+        'https://www.letras.mus.br/mais-acessadas/mpb/',
     ]
 
     def parse(self, response):
+        artist_links = response.css('.top-list_art a')
+        yield from response.follow_all(artist_links, self.parse_artist)
+
+    def parse_artist(self, response):
         song_links = response.css('.artista-todas a.song-name')
         yield from response.follow_all(song_links, self.parse_song)
 
